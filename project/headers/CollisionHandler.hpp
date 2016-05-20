c5e641c61ec5343dd29c4b54ca9e978d981f7fa2
Lukas Vollenweider <lukas.vollenweider@bluewin.ch>
2016-05-20 13:58:06 +0200

    better version of OBB

--- project/headers/CollisionHandler.hpp
+++ project/headers/CollisionHandler.hpp
@@ -8,6 +8,16 @@
 class Collisionhandler {
 public:
     Collisionhandler();
+    
+    /**
+     *  An oriented bounding box
+     *
+     */
+    struct OBB {
+        vmml::Vector2f corner[4]; /**< Corners of the box, where 0 is the lower left. */
+        vmml::Vector2f axis[2];  /**< Two edges of the box extended away from corner[0] */
+        double origin[2];
+    };
     /**
      *  Checks if an entity a collides with an entity b.
      *
@@ -15,54 +25,25 @@ public:
      *  @param b: Entity in 3D coordinate system
      *
      *  @return True if there was a collision, false otherwise
-     */
-    bool testAABBOverlap(const Entity &a, const Entity &b);
-    
-    bool testOBBOverlap(const Entity &a, const Entity &b);
-    
-    /**
-     *  Returns the collision force which affects the direction when a player collides with a collidable object
-     *
-     *  @return Vector which points orthonormal to the player
-     */
-    vmml::Vector3f getCollisionForce();
-    void applyGravity();
+     */    
+    bool testOBBOverlap(Entity &a, Entity &b);
 private:
-    enum CollisionSide { X_AXIS, X_AXIS_N, Z_AXIS, Z_AXIS_N };
-    vmml::Vector3f collisionForce;
-    vmml::Matrix<2, 2, float> boundingBoxA;
-    vmml::Matrix<2, 2, float> boundingBoxB;
-    float _collisionAngle = 0.0;
-    float _carHeight = 0.0;
-    bool gravityFlag = false;
     /**
-     *  Handles the collision.
-     *  If the player collides with a collidable object, the player slows down and the direction gets affected by the collision force.
-     *  If the player collides with a ramp, he slows down but the car get lifted
+     *  Creates an Oriented bounding box for an entity
      *
-     *  @param a: Entity in 3D coordinate system
-     *  @param b: Entity in 3D coordinate system
+     *  @param a Entity from which the OBB should be created
+     *  @return OBB An oriented bounding box
      */
-    void handleCollision(const Entity &a, const Entity &b);
+    OBB createOBB(const Entity &a);
     
     /**
-     *  Get the side from entity b with which entity a collides.
+     *  Checks if OBB a has overlapping axes with OBB b
      *
-     *  @param a: Entity in 3D coordinate system
-     *  @param b: Entity in 3D coordinate system
-     *  @return CollisionSide (X_AXIS, X_AXIS_N, Z_AXIS, Z_AXIS_N), where N means that the normal vector which is orthogonal to entity a has to be negative.
+     *  @param a OBB for entity a
+     *  @param b OBB for entity b
+     *  @return True if there are overlapping, false otherwise
      */
-    CollisionSide getCollisionSide(const Entity &a, const Entity &b);
-    
-    /**
-     *  Defines a rectangular area around the entity
-     *
-     *  @param a: Entity in 3D coordinate system
-     *  @return 2x2 Matrix which represents the rectangular area.
-     *          The first row contains the x values (min, max)
-     *          The second row contains the z values (min, max)
-     */
-    vmml::Matrix<2, 2, float> getBoundingBox(const Entity &a);
+    bool overlaps(const OBB &a, const OBB &b);
 };
 
 #endif /* CollisionHandler_hpp */
--- project/implementation/CollisionHandler.cpp
+++ project/implementation/CollisionHandler.cpp
@@ -5,147 +5,63 @@ Collisionhandler::Collisionhandler() {
 }
 
 #pragma mark Collision Test
-bool Collisionhandler::testOBBOverlap(const Entity &a, const Entity &b) {
+
+bool Collisionhandler::testOBBOverlap(Entity &a, Entity &b) {
     if (b.getType() == Entity::NOTCOLLIDABLE) {
         return false;
     }
     
-    vmml::Vector2f axis1(std::cos(a.getRotation()), std::sin(a.getRotation()));
-    vmml::Vector2f axis2(-std::sin(a.getRotation()), std::cos(a.getRotation()));
-    vmml::Vector2f axis3(std::cos(b.getRotation()), std::sin(b.getRotation()));
-    vmml::Vector2f axis4(-std::sin(b.getRotation()), std::cos(b.getRotation()));
+    OBB aOBB = createOBB(a);
+    OBB bOBB = createOBB(b);
     
-    vmml::Vector2f distance = vmml::Vector2f(a.getX(), a.getZ()) - vmml::Vector2f(b.getX(), b.getZ());
-    
-    float r1, r2, r3, r4;
-    
-    //projection on axis 1
-    r1 = a.getLength() * std::fabs(axis1.dot(axis1));
-    r2 = a.getWidth() * std::fabs(axis2.dot(axis1));
-    r3 = b.getLength() * std::fabs(axis3.dot(axis1));
-    r4 = b.getWidth() * std::fabs(axis4.dot(axis1));
-    if (r1 + r2 + r3 + r4 <= std::fabs(distance.dot(axis1))) {return false;};
-    
-    //projection on axis 2
-    r1 = a.getLength() * std::fabs(axis1.dot(axis2));
-    r2 = a.getWidth() * std::fabs(axis2.dot(axis2));
-    r3 = b.getLength() * std::fabs(axis3.dot(axis2));
-    r4 = b.getWidth() * std::fabs(axis4.dot(axis2));
-    if (r1 + r2 + r3 + r4 <= std::fabs(distance.dot(axis2))) {return false;};
-    
-    //projection on axis 3
-    r1 = a.getLength() * std::fabs(axis1.dot(axis3));
-    r2 = a.getWidth() * std::fabs(axis2.dot(axis3));
-    r3 = b.getLength() * std::fabs(axis3.dot(axis3));
-    r4 = b.getWidth() * std::fabs(axis4.dot(axis3));
-    if (r1 + r2 + r3 + r4 <= std::fabs(distance.dot(axis3))) {return false;};
-    
-    //projection on axis 4
-    r1 = a.getLength() * std::fabs(axis1.dot(axis4));
-    r2 = a.getWidth() * std::fabs(axis2.dot(axis4));
-    r3 = b.getLength() * std::fabs(axis3.dot(axis4));
-    r4 = b.getWidth() * std::fabs(axis4.dot(axis4));
-    if (r1 + r2 + r3 + r4 <= std::fabs(distance.dot(axis4))) {return false;};
-    
-    return true;
-}
-
-#pragma mark Collision Handling
-
-void Collisionhandler::handleCollision(const Entity &a, const Entity &b) {
-    switch (b.getType()) {
-        case Entity::COLLIDABLE:
-            switch (getCollisionSide(a, b)) {
-                case X_AXIS:
-                    collisionForce = vmml::Vector3f(0,0,b.getZ());
-                    break;
-                case X_AXIS_N:
-                    collisionForce = vmml::Vector3f(0,0,-b.getZ());
-                    break;
-                case Z_AXIS:
-                    collisionForce = vmml::Vector3f(b.getX(), 0, 0);
-                    break;
-                case Z_AXIS_N:
-                    collisionForce = vmml::Vector3f(-b.getX(), 0, 0);
-                    break;
-            }
-            break;
-        default:
-            break;
-    }
-}
-
-vmml::Vector3f Collisionhandler::getCollisionForce() {
-    collisionForce = vmml::normalize(collisionForce);
-    return collisionForce;
-}
-
-#pragma mark TODO - Implement realistic gravity force
-void Collisionhandler::applyGravity() {
-    if (gravityFlag) {
-        _collisionAngle = (std::abs(_collisionAngle) - 0.1)*-1;
-        std::cout << "collision angle: " << _collisionAngle << std::endl;
-        _carHeight = _carHeight - 0.1;
-    }
-    if (std::abs(_carHeight) < 0.2) {
-        gravityFlag = false;
-        _carHeight = 0.0;
-        _collisionAngle = 0.0;
-    }
+    return overlaps(aOBB, bOBB) && overlaps(bOBB, aOBB);
 }
 
 #pragma mark Helper functions
-
-vmml::Matrix<2, 2, float> Collisionhandler::getBoundingBox(const Entity &a) {
-    vmml::Matrix<2, 2, float> boundingBox;
+Collisionhandler::OBB Collisionhandler::createOBB(const Entity &a) {
+    Collisionhandler::OBB a_OBB;
+    vmml::Vector2f a_x (cosf(a.getRotation()), sinf(a.getRotation()));
+    vmml::Vector2f a_z (-sinf(a.getRotation()), cosf(a.getRotation()));
     
-    boundingBox.at(0, 0) = a.getX() - a.getWidth() / 2 - a.getLength() / 2;
-    boundingBox.at(0, 1) = a.getX() + a.getWidth() / 2 + a.getLength() / 2;
-    boundingBox.at(1, 0) = a.getZ() - a.getWidth() / 2 - a.getLength() / 2;
-    boundingBox.at(1, 1) = a.getZ() + a.getWidth() / 2 + a.getLength() / 2;
+    a_x *= a.getWidth()/2;
+    a_z *= a.getLength()/2;
     
-    return boundingBox;
+    a_OBB.corner[0] = vmml::Vector2f(a.getX(), a.getZ()) - a_x - a_z;
+    a_OBB.corner[1] = vmml::Vector2f(a.getX(), a.getZ()) + a_x - a_z;
+    a_OBB.corner[2] = vmml::Vector2f(a.getX(), a.getZ()) + a_x + a_z;
+    a_OBB.corner[3] = vmml::Vector2f(a.getX(), a.getZ()) - a_x + a_z;
+    
+    a_OBB.axis[0] = a_OBB.corner[1] - a_OBB.corner[0];
+    a_OBB.axis[1] = a_OBB.corner[3] - a_OBB.corner[0];
+    
+    for (int i = 0; i < 2; i++) {
+        a_OBB.axis[i] /= a_OBB.axis[i].squared_length();
+        a_OBB.origin[i] = a_OBB.corner[0].dot(a_OBB.axis[i]);
+    }
+    
+    return a_OBB;
 }
 
-
-Collisionhandler::CollisionSide Collisionhandler::getCollisionSide(const Entity &a, const Entity &b) {
-    float x_left = a.getX() - a.getWidth()/2;
-    float x_right = (a.getX() + a.getWidth()/2);
-    float z_up = (a.getZ() + a.getLength()/2);
-    float z_down = a.getZ() - a.getLength()/2 ;
-    
-    float min_x = boundingBoxB.at(0,0);
-    float max_x = boundingBoxB.at(0,1);
-    float min_z = boundingBoxB.at(1,0);
-    float max_z = boundingBoxB.at(1,1);
-    
-    if ((x_left >= min_x || x_right <= max_x) && (z_up <= min_z || z_down >= max_z)) {
-        if (a.getZ() < 0) {
-            if (z_up <= min_z) {
-                return X_AXIS;
-            } else {
-                return X_AXIS_N;
-            }
-        } else {
-            if (z_up <= min_z) {
-                return X_AXIS_N;
-            } else {
-                return X_AXIS;
+bool Collisionhandler::overlaps(const OBB &a, const OBB &b) {
+    for (int i = 0; i < 2; i++) {
+        double t = b.corner[0].dot(a.axis[i]);
+        
+        double tMin = t;
+        double tMax = t;
+        
+        for (int c = 0; c < 4; c++) {
+            t = b.corner[c].dot(a.axis[i]);
+            
+            if (t < tMin) {
+                tMin = t;
+            } else if (t > tMax) {
+                tMax = t;
             }
         }
-    } else {
-        if (a.getX() < 0) {
-            if (x_right <= min_x) {
-                return Z_AXIS;
-            } else {
-                return Z_AXIS_N;
-            }
-        } else {
-            if (x_right <= min_x) {
-                return Z_AXIS_N;
-            } else {
-                return Z_AXIS;
-            }
+        
+        if ((tMin > 1 + a.origin[i]) || (tMax < a.origin[i])) {
+            return false;
         }
     }
+    return true;
 }
